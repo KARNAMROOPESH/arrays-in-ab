97a2e14807217d1b7d9067335cb53463539a543a
KARNAMROOPESH <karnamroop1234@gmail.com>
2020-05-04 19:43:50 +0530

    created arrays and used it for smoke  proudused by the bird

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimg = loadImage("sprites/smoke.png");
+    this.trajactory = [ ];
   }
 
   display() {
@@ -9,5 +11,13 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+   if(this.body.position.x>220&&
+      this.body.velocity.x>5)
+     { var  position = [this.body.position.x , this.body.position.y];
+   this.trajactory.push(position);}
+   for(var i = 0; i<this.trajactory.length; i=i+1 ){
+      image(this.smokeimg,this.trajactory[i][0],this.trajactory[i][1]);
+   }
+
   }
 }
